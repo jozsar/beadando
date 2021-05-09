999503b5bc0897c1422f845760c7d21aaf8ee8ea
jozsar <jozsa.rafael@gmail.com>
2021-05-09 22:40:53 +0200

    48

--- 48_feladat.py
+++ 48_feladat.py
@@ -1,10 +1,11 @@
 import numpy as np
 import random
-kep=np.random.randint(0,2,(5,20))
+kep=np.random.randint(0,2,(5,35))
 nulla=np.random.randint(0,1,kep.shape[1])
 nulla[random.randint(0,kep.shape[1]-1)]=1
 for i in range(1,kep.shape[0],2):
-    husszazalek=np.array([np.random.randint(0, 2, kep.shape[1]), np.random.randint(0, 2, kep.shape[1]), np.random.randint(0, 2, kep.shape[1]), np.random.randint(0, 2, kep.shape[1]), nulla])
+    husszazalek=np.array([np.random.randint(0, 2, kep.shape[1]), np.random.randint(0, 2, kep.shape[1]),
+                          np.random.randint(0, 2, kep.shape[1]), np.random.randint(0, 2, kep.shape[1]), nulla])
     np.random.shuffle(husszazalek)
     kep[i:i+1:]=husszazalek[:1:]
 print(kep)
